b5300a366d74ad18e7f8b7bc6cb7ecc2b3eeff1e
Kavu0306 <kaviyaradha2000@gmail.com>
2022-08-04 12:50:29 +0530

    changes bg and alignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list, setlist] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  let addtask = () => {
+    setlist([
+      ...list,
+      { id: list.length + 1, name: currentTask, isDone: false },
+    ]);
+    setCurrentTask("");
+  };
+  let markDone = (id) => {
+    let itemIndex = list.findIndex((obj) => obj.id === id);
+    list[itemIndex].isDone = !list[itemIndex].isDone;
+    setlist([...list]);
+  };
+  // let markUndone = (id) => {
+  //   let itemIndex = list.findIndex((obj) => obj.id === id);
+  //   list[itemIndex].isDone = false;
+  //   setlist([...list]);
+  // };
+  const removetask = (id) => {
+    let itemIndex = list.findIndex((obj) => obj.id === id);
+    console.log(itemIndex);
+    let newList = list;
+    newList.splice(itemIndex, 1);
+    setlist([...newList]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task">
+      <input
+        type="text"
+        value={currentTask}
+        onChange={(e) => setCurrentTask(e.target.value)}
+      ></input>
+      <button onClick={addtask}>add</button>
+      <ul>
+        {list.map((inn, index) => {
+          return (
+            <li key={index} className={inn.isDone ? "mark-done" : ""}>
+              {inn.name}-{""}
+              <button onClick={() => markDone(inn.id)}>Done</button>-
+              {/* <button onClick={() => markUndone(inn.id)}>UnDone</button> */}
+              <button onClick={() => removetask(inn.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
 export default App;
